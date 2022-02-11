ea7d66a966f4c391c136988b1849381c0976891d
Orhan Celik <51966088+orhancelik1@users.noreply.github.com>
2022-02-11 15:52:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix = ["ec!","Ec!","EC!","eC!"],case_insensitive=True)
 client = discord.Client()
-benzin = "Benzini Shell'den yarrağı kelden diyorlar doğru mu abla? Bilmem doğrudur herhalde!"
-kaan = "Ben Orhan'ın karısıyım. Bana ondan başkası sahip olamaz!"
-orhan = "Kaan'ın kocası. Boş zamanlarını onu pompalamakla geçirir."
 
 bad_words = ["amk", "sikim", "siktir", "yarak", "yarrak", "oc","oç", "amcik","amcık","orospu",
              "orosbu", "aq", "sikik", "sikeyim", "pic", "piç", "fahişe", "fahise",
@@ -20,6 +17,7 @@ chuck_words = ["Yüce Chuck seni kutsasın pis günahkar!",
                "Yüce Chuck öfkene hakim olmanı emreder!",
                "Yüce Chuck tarafından aforoz edildiniz!",
                "Yüce Chuck ahlaklı bir birey olmanı yeğler!"]
+
 hi_words = ["$selam","$sa","$merhaba","$selamun","$selamin"]
 
 chat_words = ["$naber", "$nbr", "$nasilsin", "$nasılsın"]
@@ -32,7 +30,7 @@ def get_quote():
     translator = Translator()
     word = json_data[0]['q']
 
-    translation = translator.translate(word, src="en", dest='tr')
+    translation = translator.translate(word, src="en", dest='tr') 
     quote = translation.text + " -" + json_data[0]['a']
     return (quote)
 
@@ -48,17 +46,6 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    msg = message.content
-    if msg.startswith('$kaan'):
-        await message.channel.send(kaan)
-
-    if msg.startswith('$orhan'):
-        await message.channel.send(orhan)
-
-    if msg.startswith('$benzin'):
-        await message.channel.send(benzin)
-
-
 
     msg = message.content.lower()
     if any(word in msg for word in bad_words):
@@ -67,12 +54,10 @@ async def on_message(message):
         quote = get_quote()
         await message.channel.send(random.choice(chuck_words) + author +"\n"+ quote)
 
-    msg = message.content.lower()
     if any(word in msg for word in hi_words):
         author = message.author.mention
         await message.channel.send("selam " + author + "!")
 
-    msg = message.content.lower()
     if any(word in msg for word in chat_words):
         author = message.author.mention
         await message.channel.send("Harika! Sen? " + author + "!")
